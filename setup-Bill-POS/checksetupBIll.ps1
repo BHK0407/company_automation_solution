f0d2aae94ec4e67de949c79220d7b26da02830d2
Khang Bui <152870369+BHK0407@users.noreply.github.com>
2025-05-08 23:46:28 +0700

    Update checksetupBIll.ps1

--- setup-Bill-POS/checksetupBIll.ps1
+++ setup-Bill-POS/checksetupBIll.ps1
@@ -12,9 +12,9 @@ if (
     $currentSettings.margin_top -eq "0.5" -and
     $currentSettings.margin_bottom -eq "0.5"
 ) {
-    Write-Host "Cau hinh da duoc ap dung day du. " -ForegroundColor Green
+    Write-Host "Page Setting is already. " -ForegroundColor Green
     Start-Sleep -Seconds 2
 } else {
-    Write-Host "Cau hinh chu duoc ap dung day du hoac da bi thay doi." -ForegroundColor Red
+    Write-Host "Page Setting is not already or change configuration" -ForegroundColor Red
     Start-Sleep -Seconds 2
 }
