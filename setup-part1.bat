c9317459d0dfb5ae5d9a834f5c00b411644397cb
Khang Bui <152870369+BHK0407@users.noreply.github.com>
2025-05-16 18:19:18 +0700

    Add files via upload

---
+++ setup-part1.bat
@@ -0,0 +1,78 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: === Thiết lập log file ===
+set "logfile=%~dp0setup-part1.log"
+echo [%DATE% %TIME%] === Starting Setup Part 1 === > "%logfile%" 2>&1
+
+:: Ping để delay chút
+call :log "Delaying with ping..."
+ping 127.0.0.1 -n 3 > nul
+
+:: Cấu hình IP, tạo user, đổi tên máy, v.v.
+call :log "Running setupIP.exe..."
+start "" /wait "%~dp0info\setupIP.exe" >> "%logfile%" 2>&1
+
+call :log "Reading user & IP from text files..."
+set /p user=<%~dp0info\user.txt
+set /p ip=<%~dp0info\ip.txt
+
+call :log "Setting static IP and DNS..."
+netsh int ipv4 set address name="Ethernet" static %ip% >> "%logfile%" 2>&1
+netsh int ipv4 set dns name="Ethernet" static 10.0.0.5 >> "%logfile%" 2>&1
+netsh int ipv4 add dns name="Ethernet" 8.8.8.8 index=2 >> "%logfile%" 2>&1
+powershell -command Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6 >> "%logfile%" 2>&1
+
+:: BlockFirstSignIn
+call :log "Importing First Sign-In registry settings..."
+reg import "%~dp0BlockFirstSignIn\Disable_Privacy_settings_experience_on_user_logon.reg" >> "%logfile%" 2>&1
+reg import "%~dp0BlockFirstSignIn\Disable_User_First_Sign-in_Animation.reg" >> "%logfile%" 2>&1
+
+:: TaskbarEdit
+call :log "Disabling News and Interests..."
+start /min "" %~dp0TaskbarEdit\DisableNewsAndInterests.vbs >> "%logfile%" 2>&1
+timeout /t 5 >nul
+
+call :log "Disabling Task View Button..."
+start /min "" %~dp0TaskbarEdit\DisableTaskViewButton.vbs >> "%logfile%" 2>&1
+timeout /t 5 >nul
+
+call :log "Hiding Search Box..."
+start /min "" %~dp0TaskbarEdit\HiddenSearch.vbs >> "%logfile%" 2>&1
+timeout /t 5 >nul
+
+call :log "Importing taskbar reset registry..."
+reg import "%~dp0TaskbarEdit\Disable_and_Hide_pinned_apps_on_taskbar.reg" >> "%logfile%" 2>&1
+
+:: Hidden search Box via PowerShell
+call :log "Executing SearchBox.ps1..."
+powershell -ExecutionPolicy Bypass -File "%~dp0TaskbarEdit\SearchBox.ps1" >> "%logfile%" 2>&1
+timeout /t 2 >nul
+
+:: User creation and rename
+call :log "Creating user %user%..."
+net user %user% /add >> "%logfile%" 2>&1
+net user %user% /passwordchg:no >> "%logfile%" 2>&1
+wmic UserAccount where Name='%user%' set PasswordExpires=False >> "%logfile%" 2>&1
+WMIC Computersystem where Name='%computername%' Call Rename Name='%user%' >> "%logfile%" 2>&1
+
+:: Flag
+call :log "Writing flag file after_reboot.flag..."
+echo done > "%~dp0after_reboot.flag"
+
+:: RunOnce setup for part 2
+call :log "Registering setup-part2.bat with RunOnce..."
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v ContinuePOSSetup /t REG_SZ /d "\"%~dp0setup-part2.bat\"" /f >> "%logfile%" 2>&1
+
+:: Restart
+call :log "Restarting system in 5 seconds..."
+shutdown /r /t 5 /c "System will restart to apply taskbar changes." /f
+
+goto :eof
+
+:: === Hàm ghi log có timestamp ===
+:log
+set "ts=[%DATE% %TIME%]"
+echo !ts! %~1 >> "%logfile%"
+echo %~1
+goto :eof
---
+++ setup-part2.bat
@@ -0,0 +1,125 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: === Thiết lập log file ===
+set "logfile=%~dp0setup-part2.log"
+echo [%DATE% %TIME%] === Starting Setup Part 2 === > "%logfile%" 2>&1
+
+:: Kiểm tra flag tồn tại để chắc chắn chỉ chạy sau khi reboot
+if not exist "%~dp0after_reboot.flag" (
+    call :log "Flag file after_reboot.flag does not exist, exiting."
+    exit /b
+)
+
+call :log "Starting setup-part2.bat..."
+
+call :log "Installing 7zip..."
+start /wait "%~dp0\7z1900-x64.exe" /S >> "%logfile%" 2>&1
+
+call :log "Installing NodeJS..."
+start /wait msiexec.exe /i "%~dp0\nodejs\node.msi" /quiet >> "%logfile%" 2>&1
+
+call :log "Installing WebView2..."
+start /wait "%~dp0\webview\WebView2X64.exe" /silent /install >> "%logfile%" 2>&1
+
+call :log "Installing OneDrive..."
+start /wait "%~dp0\onedrive\OneDriveSetup.exe" /silent /allusers >> "%logfile%" 2>&1
+
+call :log "Installing SQL Server Express..."
+start /wait %~dp0\SQLEXPRADV_x64_ENU\setup.exe /Q /HIDECONSOLE=1 /ACTION=INSTALL /IACCEPTSQLSERVERLICENSETERMS=1 /FEATURES=SQLENGINE,Tools /UPDATEENABLED=0 /INSTANCENAME="sqlexpress" /ADDCURRENTUSERASSQLADMIN=1 /SECURITYMODE=SQL /SKIPRULES=RebootRequiredCheck /SAPWD="gs25@localadmin" /SQLSVCSTARTUPTYPE=AUTOMATIC /BROWSERSVCSTARTUPTYPE=AUTOMATIC >> "%logfile%" 2>&1
+
+call :log "Importing registry environment settings..."
+reg import "%~dp0\invaironment\invaironment_win10.reg" >> "%logfile%" 2>&1
+regedit.exe /S "%~dp0\invaironment\Disable_Language_page_in_Settings.reg" >> "%logfile%" 2>&1
+
+call :log "Installing .NET Framework 4.6.1..."
+start /wait "%~dp0\NetFramwork4.6.1.exe" /q /norestart >> "%logfile%" 2>&1
+
+call :log "Installing SQL Server Management Studio..."
+start /wait "%~dp0\SQLEXPRADV_x64_ENU\SSMS-Setup-ENU.exe" /install /quiet /norestart >> "%logfile%" 2>&1
+
+call :log "Installing SQL Cmd Line Utils..."
+start /wait msiexec /i "%~dp0\MsSqlCmdLnUtils.msi" /passive IACCEPTMSSQLCMDLNUTILSLICENSETERMS=YES >> "%logfile%" 2>&1
+
+call :log "Copying Group Policy..."
+xcopy "%~dp0\GroupPolicy" "C:\Windows\System32\GroupPolicy" /i /e /s /y >> "%logfile%" 2>&1
+
+call :log "Copying shortcut UpdatorMessage.lnk to Startup..."
+xcopy "%~dp0\UpdatorMessage.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup" >> "%logfile%" 2>&1
+
+call :log "Copying hosts file..."
+xcopy "%~dp0\hosts" "C:\Windows\System32\drivers\etc\" /Y >> "%logfile%" 2>&1
+
+call :log "Setting timezone and synchronizing time..."
+tzutil /s "SE Asia Standard Time" >> "%logfile%" 2>&1
+reg add "HKCU\Control Panel\International" /V sShortDate /T REG_SZ /D "dd/MM/yyyy" /F >> "%logfile%" 2>&1
+w32tm /config /syncfromflags:manual /manualpeerlist:time1.google.com >> "%logfile%" 2>&1
+net stop w32time >> "%logfile%" 2>&1
+net start w32time >> "%logfile%" 2>&1
+w32tm /config /update >> "%logfile%" 2>&1
+w32tm /resync >> "%logfile%" 2>&1
+
+call :log "Disabling UAC..."
+reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f >> "%logfile%" 2>&1
+
+call :log "Importing Firewall rules..."
+netsh advfirewall import "%~dp0\firewall-rule.wfw" >> "%logfile%" 2>&1
+
+call :log "Running PowerShell script to edit SQL service..."
+powershell -ExecutionPolicy Bypass -File "%~dp0\edit-sqlservice.ps1" >> "%logfile%" 2>&1
+
+call :log "Stopping and disabling Windows Update service..."
+net stop wuauserv >> "%logfile%" 2>&1
+sc config wuauserv start= disabled >> "%logfile%" 2>&1
+
+call :log "Disabling Scheduled Defrag task..."
+schtasks /Change /DISABLE /TN "\Microsoft\Windows\Defrag\ScheduledDefrag" >> "%logfile%" 2>&1
+
+call :log "Setting Administrator password..."
+net user administrator gs25@localadmin >> "%logfile%" 2>&1
+
+call :log "Preparing POS bill form setup..."
+set /p user=<"%~dp0info\user.txt"
+call :log "User name: %user%"
+
+call :log "Setting auto login for user %user%..."
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f >> "%logfile%" 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUsername /t REG_SZ /d %user% /f >> "%logfile%" 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d "" /f >> "%logfile%" 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultDomainName /t REG_SZ /d %COMPUTERNAME% /f >> "%logfile%" 2>&1
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 0 /f >> "%logfile%" 2>&1
+
+call :log "Copying check.exe to C:\Users\Public\..."
+copy "%~dp0check.exe" "C:\Users\Public\" /Y >> "%logfile%" 2>&1
+
+call :log "Creating run_once.bat to run check.exe after user login..."
+(
+echo @echo off
+echo timeout /t 2 /nobreak
+echo start /wait "" "C:\Users\Public\check.exe"
+echo del "C:\Users\Public\check.exe"
+echo del "C:\Users\Public\run_once.bat"
+echo powershell -Command "Remove-Item '%%ProgramData%%\Microsoft\Windows\Start Menu\Programs\Startup\run_once.lnk' -Force"
+) > C:\Users\Public\run_once.bat
+
+call :log "Creating shortcut run_once.lnk in Startup folder..."
+powershell -ExecutionPolicy Bypass -Command ^
+ "$s='C:\Users\Public\run_once.bat'; $l='C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\run_once.lnk'; $w=New-Object -ComObject WScript.Shell; $sc=$w.CreateShortcut($l); $sc.TargetPath=$s; $sc.WorkingDirectory='C:\Users\Public'; $sc.Save()" >> "%logfile%" 2>&1
+
+call :log "Displaying run_once.bat content..."
+type C:\Users\Public\run_once.bat >> "%logfile%" 2>&1
+
+call :log "Deleting flag file after_reboot.flag..."
+del "%~dp0after_reboot.flag" >> "%logfile%" 2>&1
+
+call :log "Restarting computer in 5 seconds..."
+shutdown /r /t 5 /c "Restarting to auto-login with user %user%..." /f
+
+goto :eof
+
+:: === Hàm ghi log có timestamp ===
+:log
+set "ts=[%DATE% %TIME%]"
+echo !ts! %~1 >> "%logfile%"
+echo %~1
+goto :eof
