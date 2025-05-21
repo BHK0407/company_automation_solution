93c2f78b872f460e1c94c9d68268113e4543d242
Khang Bui <152870369+BHK0407@users.noreply.github.com>
2025-05-21 18:51:05 +0700

    Add files via upload

---
+++ checkSecurityWarning-SmartScreen.ps1
@@ -0,0 +1,7 @@
+﻿Get-ChildItem "%~dp0" -Recurse | ForEach-Object {
+    if (Get-Item $_.FullName -Stream "Zone.Identifier" -ErrorAction SilentlyContinue) {
+        Write-Host "$($_.FullName) is BLOCKED" -ForegroundColor Red
+    } else {
+        Write-Output "$($_.FullName) is OK"
+    }
+}
